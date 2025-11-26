59d98839fe63e23e0ebdfff13e6d75857b0e45a0
Yehuda Linker! <uda.linker@gmail.com>
2025-11-26 11:32:31 +0200

    Upload a working servUpload a working serverer

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import uvicorn 
+from src.routes.routes import router
+
+app = FastAPI()
+
+app.include_router(router)
+
+
+if __name__=="__main__":
+    uvicorn.run(app , host="localhost", port=8000)
---
+++ src/routes/routes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+
+@router.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
