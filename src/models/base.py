977bc1663d71ad2e5f3dadce4f1eb6a53e2310a9
Yehuda Linker! <uda.linker@gmail.com>
2025-11-26 16:59:49 +0200

    Finish building a base with houses and rooms

--- src/models/base.py
+++ src/models/base.py
@@ -13,3 +13,9 @@ class Base:
             if i.house_num == house_num:
                 self.houses.remove(i)
     
+
+# class Base:
+#     def __init__(self, base_name: str):
+#         self.base_name=base_name
+#         self.houses=list[hash]
+#         self.rooms=list[Room]
